4f26bb745e093099214541f5b8fd445a0e537e7a
Wil Wade <wil.wade@amplica.io>
2024-09-06 08:44:29 -0400

    Adding the schema urls

--- libraries/js/src/x25519.ts
+++ libraries/js/src/x25519.ts
@@ -1,9 +1,9 @@
 import { hexToU8a, u8aToHex } from '@polkadot/util';
-import { box } from 'tweetnacl';
+import nacl from 'tweetnacl';
 
 export function isValidX25519PrivateKey(privateKey: string, testPublicKey: string): boolean {
   try {
-    const { publicKey } = box.keyPair.fromSecretKey(hexToU8a(privateKey));
+    const { publicKey } = nacl.box.keyPair.fromSecretKey(hexToU8a(privateKey));
     return u8aToHex(publicKey).toLowerCase() === testPublicKey.toLowerCase();
   } catch (_e) {
     return false;
