d8a8ee08c5261dec4ffdac76e81a23cfcd072867
Wil Wade <wil.wade@amplica.io>
2024-09-30 15:33:52 +0000

    Fix build issue

--- tools/signed-request-generator/vite.config.ts
+++ tools/signed-request-generator/vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -8,5 +9,14 @@ export default defineConfig({
 		minify: true, // Minify the JS and CSS
 		assetsInlineLimit: 100000000, // Large limit to inline all assets
 		chunkSizeWarningLimit: 100000000
+	},
+	resolve: {
+		alias: {
+			// https://github.com/digitalcredentials/open-badges-context/issues/10
+			'@digitalcredentials/open-badges-context': path.resolve(
+				__dirname,
+				'node_modules/@digitalcredentials/open-badges-context/dist/context.js'
+			)
+		}
 	}
 });
